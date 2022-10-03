09d83f8b80d626d384966148685bc5648b459d35
nthprt <114790885+nthprt@users.noreply.github.com>
2022-10-02 21:03:00 -0300

    Actualización de TCPServerHilo.java

--- TCP-Type/src/main/java/py/una/server/tcp/TCPServerHilo.java
+++ TCP-Type/src/main/java/py/una/server/tcp/TCPServerHilo.java
@@ -21,10 +21,11 @@ import java.text.SimpleDateFormat;
 public class TCPServerHilo extends Thread {
 
     private Socket socket = null;
-
+    
     TCPMultiServer servidor;
-    List<Nis> usuarios = new ArrayList<>(); //almacenar una lista de usuarios (no se utiliza, se deja para que el alumno lo utilice)
+    List<Nis> usuarios = new ArrayList<Nis>(); //almacenar una lista de usuarios (no se utiliza, se deja para que el alumno lo utilice)
     List<Logger> logs = new ArrayList<Logger>();
+    
     public TCPServerHilo(Socket socket, TCPMultiServer servidor ) {
         super("TCPServerHilo");
         this.socket = socket;
@@ -43,82 +44,89 @@ public class TCPServerHilo extends Thread {
             Integer NisNro=0;
             inputLine = in.readLine();
             boolean encuentra= false;
-            Peticion peticion = PeticionJSON.stringObjeto(inputLine);
-            Integer tipoOperacion = peticion.getOperacion();
-            Peticion respuesta = new Peticion(0,"","",0);
+            
+            
+            //
             JSONObject respuestaJson = new JSONObject();
-            if (tipoOperacion == 1){
-                // Registrar consumo
-            	try {
-	                Nis nis = NisJSON.stringObjeto(inputLine);
-	                usuarios.add(nis);
-	                respuesta.setMensaje("OK");
-	                respuesta.setEstado(0);
-	                respuesta.setOperacion(1);
-	                respuesta.setDatos(inputLine);
-	                System.exit(0);
-            	}
-            	catch(Exception e) {
-            		respuesta.setMensaje(e.toString());
-	                respuesta.setEstado(-1);
-	                respuesta.setOperacion(1);
-            	}
-            }
-            else if (tipoOperacion == 2){
-            	try {
-	                respuesta.setMensaje("OK");
-	                respuesta.setEstado(0);
-	                respuesta.setOperacion(2);
-            	}catch (Exception e){
-            		respuesta.setMensaje(e.toString());
-	                respuesta.setEstado(-1);
-	                respuesta.setOperacion(2);
-            	}
-            }
-            else if (tipoOperacion == 3){
-                // Listar nis activos
-            	try {
-                	String datosNis = "[";
-	                for (Nis nis : usuarios){
-	                    if (nis.getEsActivo()){
-	                        datosNis += NisJSON.objetoString(nis) + ",";
-	                    }
-	                }
-	                datosNis += "]";
-	                respuesta.setDatos(datosNis);
-	                respuesta.setMensaje("OK");
-	                respuesta.setEstado(0);
-	                respuesta.setOperacion(3);
-	                outputLine = respuestaJson.toJSONString();
-            	}catch (Exception e) {
-            		respuesta.setMensaje(e.toString());
-	                respuesta.setEstado(-1);
-	                respuesta.setOperacion(3);
-            	}
-            }
-            else if (tipoOperacion == 4){
-                // Listar nis inactivos
-            	try {
-	                String datosNis = "[";
-	                for (Nis nis : usuarios){
-	                    if (!nis.getEsActivo()){
-	                        datosNis += NisJSON.objetoString(nis)+",";
-	                    }
-	                }
-	                datosNis += "]";
-	                respuesta.setDatos(datosNis);
-	                respuesta.setMensaje("OK");
-	                respuesta.setEstado(0);
-	                respuesta.setOperacion(4);
-            	} catch(Exception e) {
-            		respuesta.setMensaje(e.toString());
-	                respuesta.setEstado(-1);
-	                respuesta.setOperacion(4);
-            	}
-            }
-            else{
-                respuesta.setDatos("Operacion no valida");
-                respuesta.setEstado(1);
+            while ((inputLine = in.readLine()) != null) {
+            	System.out.println("esperando tcp");
+            	Peticion peticion = PeticionJSON.stringObjeto(inputLine);
+                Integer tipoOperacion = peticion.getOperacion();
+            	Peticion respuesta = new Peticion(0,"","",0);
+
+	            if (tipoOperacion == 1){
+	                // Registrar consumo
+	            	try {
+		                Nis nis = NisJSON.stringObjeto(inputLine);
+		                usuarios.add(nis);
+		                respuesta.setMensaje("OK");
+		                respuesta.setEstado(0);
+		                respuesta.setOperacion(1);
+		                respuesta.setDatos(inputLine);
+		                System.exit(0);
+	            	}
+	            	catch(Exception e) {
+	            		respuesta.setMensaje(e.toString());
+		                respuesta.setEstado(-1);
+		                respuesta.setOperacion(1);
+	            	}
+	            }
+	            else if (tipoOperacion == 2){
+	            	try {
+		                respuesta.setMensaje("OK");
+		                respuesta.setEstado(0);
+		                respuesta.setOperacion(2);
+	            	}catch (Exception e){
+	            		respuesta.setMensaje(e.toString());
+		                respuesta.setEstado(-1);
+		                respuesta.setOperacion(2);
+	            	}
+	            }
+	            else if (tipoOperacion == 3){
+	                // Listar nis activos
+	            	try {
+	                	String datosNis = "[";
+		                for (Nis nis : usuarios){
+		                    if (nis.getEsActivo()){
+		                        datosNis += NisJSON.objetoString(nis) + ",";
+		                    }
+		                }
+		                datosNis += "]";
+		                respuesta.setDatos(datosNis);
+		                respuesta.setMensaje("OK");
+		                respuesta.setEstado(0);
+		                respuesta.setOperacion(3);
+		                outputLine = respuestaJson.toJSONString();
+	            	}catch (Exception e) {
+	            		respuesta.setMensaje(e.toString());
+		                respuesta.setEstado(-1);
+		                respuesta.setOperacion(3);
+	            	}
+	            }
+	            else if (tipoOperacion == 4){
+	                // Listar nis inactivos
+	            	try {
+		                String datosNis = "[";
+		                for (Nis nis : usuarios){
+		                    if (!nis.getEsActivo()){
+		                        datosNis += NisJSON.objetoString(nis)+",";
+		                    }
+		                }
+		                datosNis += "]";
+		                respuesta.setDatos(datosNis);
+		                respuesta.setMensaje("OK");
+		                respuesta.setEstado(0);
+		                respuesta.setOperacion(4);
+	            	} catch(Exception e) {
+	            		respuesta.setMensaje(e.toString());
+		                respuesta.setEstado(-1);
+		                respuesta.setOperacion(4);
+	            	}
+	            }
+	            else{
+	                respuesta.setDatos("Operacion no valida");
+	                respuesta.setEstado(1);
+	            }
             }
             String ip=socket.getRemoteSocketAddress().toString();
             int puerto= socket.getPort();
@@ -130,15 +138,17 @@ public class TCPServerHilo extends Thread {
             Date date = new Date();
             String fecha = dateFormat.format(date);
          // guarda un registro de las operaciones realizadas por los clientes
-            Logger log = new Logger(ip+puertoAux1, ip2+puertoAux2, fecha, tipoOperacion);
-            logs.add(log);
+           // Logger log = new Logger(ip+puertoAux1, ip2+puertoAux2, fecha, tipoOperacion);
+            //logs.add(log);
             out.close();
             in.close();
             socket.close();
-            System.out.println("Finalizando Hilo");
+            System.out.println("Operación Exitosa");
 
         } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
+
 }
+
